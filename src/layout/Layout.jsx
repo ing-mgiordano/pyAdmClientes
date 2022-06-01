ad60f7d98224915a4838ab2e08587d4a34c81f1b
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-01 17:30:36 +0200

    se aplico Tailwind al Layout Principal

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,10 +2,30 @@ import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
-    <div>
-        <h1>Desde Layout.jsx</h1>
+    <div className="md:flex md:min-h-screen">
+        <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+          <h2 className="text-4xl font-black text-center text-white">CMR - Clientes</h2>
+          
+          <nav className="mt-10">
 
-        <Outlet />
+            <a 
+              className="text-white text-2xl block mt-2 hover:text-blue-300"
+              href="/clientes"
+            >Clientes</a>
+
+
+            <a 
+              className="text-white text-2xl block mt-2 hover:text-blue-300"
+              href="/clientes/nuevo"
+            >Nuevo Cliente</a>
+
+          </nav>
+        </div>
+
+        <div className="md:w-3/4 bg-amber-500">
+          <Outlet />
+        </div>
+
     </div>
   )
 }
